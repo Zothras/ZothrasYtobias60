7701d351b792d7b07dadf197f17938081d820afc
INFO-4 <INFO-4@DESKTOP-8L4NFHR>
2025-04-23 20:47:28 -0300

    Agregar archivos de proyecto.

---
+++ ZothrasYtobias60/Personaje.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZothrasYTobias
+{
+    public class Personaje
+    {
+        public string Color { get; set; }
+        public int Vida { get; set; }
+        public int Defensa { get; set; }
+        public int Fuerza { get; set; }
+        public int Mana { get; set; }
+
+
+        public void Carga()
+        {
+            Console.WriteLine("Ingrese el color de su personaje");
+            Color = Console.ReadLine();
+            Console.WriteLine("Ingrese la cantidad de vida que tiene su personaje");
+            Vida = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de defensa que tiene su personaje");
+            Defensa = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de fuerza que tiene su personaje");
+            Fuerza = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de mana que tiene su personaje");
+            Mana = int.Parse(Console.ReadLine());
+        }
+
+       
+
+
+    }
+}
---
+++ ZothrasYtobias60/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Drawing;
+using ZothrasYTobias;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+void carga(Personaje p)
+{
+    Console.WriteLine("Ingrese el color de su personaje");
+    p.Color = Console.ReadLine();
+    Console.WriteLine("Ingrese la cantidad de vida que tiene su personaje");
+    p.Vida = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese la cantidad de defensa que tiene su personaje");
+    p.Defensa = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese la cantidad de fuerza que tiene su personaje");
+    p.Fuerza = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese la cantidad de mana que tiene su personaje");
+    p.Mana = int.Parse(Console.ReadLine());
+}
+
+void mostramela(Personaje p)
+{
+    Console.WriteLine("Color: " + p.Color);
+    Console.WriteLine("Vida: " + p.Vida);
+    Console.WriteLine("Defensa: " + p.Defensa);
+    Console.WriteLine("Fuerza: " + p.Fuerza);
+    Console.WriteLine("Mana: " + p.Mana);
+
+}
+
+void CambioDeColor (Personaje p) 
+{
+    string colornuevo;
+    Console.WriteLine("El color actual de su personaje (Personaje 1) es: " + p.Color);
+    Console.WriteLine("Porfavor indique cual es el nuevo color de su personaje");
+    colornuevo = Console.ReadLine();
+    p.Color = colornuevo;
+    Console.WriteLine("Su nuevo color es: " + p.Color);
+}
+
+void RecibirDaño (Personaje p) 
+{
+    int dañoquemehicieron;
+    Console.WriteLine("Aurelion sol bajo desde las estrellas y te escupio con fuego intelestelar, ¿Cuanto daño te ha hecho?");
+    dañoquemehicieron = int.Parse(Console.ReadLine());
+    p.Vida -= dañoquemehicieron;
+    Console.WriteLine("La nueva vida de su personaje es: " );
+    Console.WriteLine(" " + p.Vida);
+}
+
+void Atacar (Personaje p) 
+{
+
+
+}
+
+
+Personaje p1 = new Personaje();    
+Console.WriteLine("Carga del personaje 1");
+carga(p1);
+Personaje p2 = new Personaje();
+Console.WriteLine("Carga del personaje 2");
+carga(p2);
+Console.Clear();
+Console.WriteLine("Datos del personaje 1: ");
+mostramela(p1);
+Console.WriteLine("-----------");
+Console.WriteLine("Datos del personaje 2: ");
+mostramela(p2);
+
+Console.WriteLine("Escriba la accion que quiera hacer a continuacion teniendo en cuenta las distintas opciones, sus opciones son: ");
+Console.WriteLine("1: Cambiar Color ");
+Console.WriteLine("2: Recibir Daño ");
+Console.WriteLine("3: Atacar ");
+string accion = Console.ReadLine(); 
+
+switch (accion)
+{
+
+    case "Cambiar Color":
+        CambioDeColor(p1);
+        Console.WriteLine("Los datos de su personaje son: ");
+        mostramela(p1);
+        break;
+
+    case "Recibir Daño":
+        RecibirDaño(p1);
+        Console.WriteLine("Los datos de su personaje son: ");
+        mostramela(p1);
+        break;
+
+    case "Atacar":
+        Console.WriteLine("1: 3 ");
+
+        break;
+}
+
+Console.ReadKey();
+
